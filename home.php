9a966dcc5f6e164df8266b2e380aa789fcd92555
Sahas R <sahasr369@gmail.com>
2022-03-07 14:04:06 +0530

    final commit

--- home.php
+++ home.php
@@ -90,24 +90,6 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin']!=true){
                 <p class="card-text"></p>
             </div>
         </div>
-
-        <!-- <div class="flex-item card" style="width: 18rem;">
-            <img src="images/research.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h1>Heading</h1>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-            </div>
-        </div>
-
-        <div class="flex-item card" style="width: 18rem;">
-            <img src="images/research.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h1>Heading</h1>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-            </div>
-        </div> -->
     </div>
 
     <hr>
